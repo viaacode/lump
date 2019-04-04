d3da996bb9bbd09a774c0c846c810126b6eaf4d9
MikeSmithEU <github@mikesmith.eu>
2019-04-04 15:27:51 +0200

    fix doctest via pytest with custom log handler using stringio buffer, improve output (use repr and ignore arguments if not given)

--- src/lump/decorators.py
+++ src/lump/decorators.py
@@ -18,30 +18,50 @@ def log_call(logger: logging.Logger, log_level=logging.DEBUG, result=False):
     """
     Decorator to log all calls to decorated function to given logger
 
-    >>> import logging, sys
-    >>> logging.basicConfig(stream=sys.stdout, format='%(levelname)s:%(name)s: %(message)s')
-    >>> logger = logging.getLogger('logger_name')
-    >>> logger.setLevel(logging.DEBUG)
+    >>> import logging, sys, io
+    >>>
+    >>> def get_logger():
+    ...     logger = logging.getLogger('logger_name')
+    ...     logger.setLevel(logging.DEBUG)
+    ...     stream = io.StringIO()
+    ...     ch = logging.StreamHandler(stream)
+    ...     ch.setLevel(logging.DEBUG)
+    ...     ch.setFormatter(logging.Formatter('%(levelname)s:%(name)s: %(message)s'))
+    ...     logger.addHandler(ch)
+    ...     return logger, stream
+    >>>
+    >>> logger, stream = get_logger()
     >>> @log_call(logger, logging.WARNING)
     ... def test(*args, **kwargs):
     ...     return 'result'
     >>> test('arg1', arg2='someval', arg3='someotherval')
-    WARNING:logger_name: test(arg1, arg2=someval, arg3=someotherval)
     'result'
+    >>> print(stream.getvalue().strip())
+    WARNING:logger_name: test('arg1', arg2='someval', arg3='someotherval')
+    >>> logger, stream = get_logger()
     >>> @log_call(logger, result=True)
     ... def test(*args, **kwargs):
     ...     return 'result'
     >>> test(arg2='someval', arg3='someotherval')
-    DEBUG:logger_name: test(, arg2=someval, arg3=someotherval)
-    DEBUG:logger_name: test returned: result
     'result'
+    >>> print(stream.getvalue().strip())
+    DEBUG:logger_name: test(arg2='someval', arg3='someotherval')
+    DEBUG:logger_name: test returned: result
     """
     def _log_call(func: callable):
         def _(*args, **kwargs):
-            logger.log(log_level, '%s(%s, %s)',
-                       func.__name__,
-                       ', '.join([str(a) for a in args]),
-                       ', '.join([k + '=' + str(kwargs[k]) for k in kwargs]))
+            arguments_format = []
+            arguments_list = []
+            if len(args):
+                arguments_format.append('%s')
+                arguments_list.append(', '.join([repr(a) for a in args]))
+            if len(kwargs):
+                arguments_format.append('%s')
+                arguments_list.append(', '.join([k + '=' + repr(kwargs[k]) for k in kwargs]))
+
+            arguments_format = '%s(%s)' % (func.__name__, ', '.join(arguments_format))
+
+            logger.log(log_level, arguments_format, *arguments_list)
             result_ = func(*args, **kwargs)
             if result:
                 logger.log(log_level, '%s returned: %s', func.__name__, result_)
