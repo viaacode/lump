e3b94a08a12bd8a222c945d558a072f1da8d1e4f
MikeSmithEU <github@mikesmith.eu>
2019-07-02 17:02:24 +0200

    some more unittests

--- tests/lump/test_humanreadable.py
+++ tests/lump/test_humanreadable.py
@@ -7,6 +7,9 @@ import pytest
     (0.5, '0.50B'),
     (-34.5, '-34.50B'),
     (1000, '1.00kB'),
+    (1.56e10, '15.60GB'),
+    (43.328453e20, '4.33ZB'),
+    (-2.12345e24, '-2.12YB'),
 ])
 def test_metric_basic(num, expected_output):
     assert format_metric(num) == expected_output
@@ -17,6 +20,8 @@ def test_metric_basic(num, expected_output):
     (0.5, '0.50B'),
     (-34.5, '-34.50B'),
     (1024, '1.00KiB'),
+    (1.56e10, '14.53GiB'),
+    (1024**4, '1.00TiB'),
 ])
 def test_binary_basic(num, expected_output):
     assert format_binary(num) == expected_output
